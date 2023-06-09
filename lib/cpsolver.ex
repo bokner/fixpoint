73cd49e4be1e44d650fb0e734d3bb0d965f705a5
Boris Okner <boris.okner@gmail.com>
2023-06-09 19:49:51 -0400

    Generate app

---
+++ lib/cpsolver.ex
@@ -0,0 +1,18 @@
+defmodule Cpsolver do
+  @moduledoc """
+  Documentation for `Cpsolver`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cpsolver.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cpsolver_test.exs
@@ -0,0 +1,8 @@
+defmodule CpsolverTest do
+  use ExUnit.Case
+  doctest Cpsolver
+
+  test "greets the world" do
+    assert Cpsolver.hello() == :world
+  end
+end
