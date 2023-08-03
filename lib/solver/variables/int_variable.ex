20b6776f44cead8fe16fc5a5609ea6f508a73318
Boris Okner <boris.okner@gmail.com>
2023-08-02 22:11:57 -0400

    Store/backend/vars (wip)

--- lib/solver/variables/int_variable.ex
+++ lib/solver/variables/int_variable.ex
@@ -1,23 +1,9 @@
 defmodule CPSolver.IntVariable do
-  alias CPSolver.IntDomain
+  use CPSolver.Variable
   alias CPSolver.Variable
 
-  defdelegate dom(var), to: IntDomain
-  defdelegate min(var), to: IntDomain
-  defdelegate max(var), to: IntDomain
-  defdelegate remove(var, val), to: IntDomain
-  defdelegate removeAbove(var, val), to: IntDomain
-  defdelegate removeBelow(var, val), to: IntDomain
-
-  defdelegate fix(var, val), to: IntDomain
-
-  defdelegate new(domain), to: Variable
-
-  def fixed?(variable) do
-    IntDomain.size(variable) == 1
-  end
-
-  def fix(variable, value) do
-    IntDomain.removeAllBut(variable, value)
+  @impl true
+  def size(%Variable{domain: domain}) do
+    :gb_sets.size(domain)
   end
 end
--- lib/solver/variables/variable.ex
+++ lib/solver/variables/variable.ex
@@ -1,10 +1,87 @@
 defmodule CPSolver.Variable do
-  defstruct [:id, :name, :space, :domain]
-  @type t :: %__MODULE__{id: reference(), name: String.t(), space: any(), domain: any()}
+  defstruct [:id, :name, :space, :backend, :domain]
 
-  def new(domain, name \\ nil, space \\ nil) do
-    %__MODULE__{id: make_ref(), domain: domain, name: name, space: space}
+  @type t :: %__MODULE__{
+          id: reference(),
+          name: String.t(),
+          space: any(),
+          backend: atom(),
+          domain: any()
+        }
+
+  @callback domain(values :: Enum.t()) :: any()
+  @callback contains?(variable :: Variable.t(), value :: number()) :: boolean()
+  @callback size(variable :: Variable.t()) :: integer()
+  @callback min(variable :: Variable.t()) :: number()
+  @callback max(variable :: Variable.t()) :: number()
+  @callback remove(variable :: Variable.t(), value :: number()) :: any()
+  @callback removeAbove(variable :: Variable.t(), value :: number()) :: any()
+  @callback removeBelow(variable :: Variable.t(), value :: number()) :: any()
+  @callback fix(variable :: Variable.t(), value :: number()) :: any()
+  @callback fixed?(variable :: Variable.t()) :: boolean()
+
+  defmacro __using__(_) do
+    quote do
+      @behaviour CPSolver.Variable
+      import CPSolver.Variable
+      def new(values, name \\ nil, space \\ nil) do
+        %CPSolver.Variable{id: make_ref(), domain: domain(values), name: name, space: space}
+      end
+
+      def domain(values) do
+        Enum.reduce(values, :gb_sets.new(), fn v, acc -> :gb_sets.add_element(v, acc) end)
+      end
+
+      def size(variable) do
+        length(domain(variable))
+      end
+
+      def fixed?(variable) do
+        size(variable) == 1
+      end
+
+      def min(variable) do
+        Enum.min(domain(variable))
+      end
+
+      def max(variable) do
+        Enum.max(domain(variable))
+      end
+
+      def contains?(variable, value) do
+        backend_op(:contains?, variable, value)
+      end
+
+      def remove(variable, value) do
+        backend_op(:remove, variable, value)
+      end
+
+      def removeAbove(variable, value) do
+        backend_op(:removeAbove, variable, value)
+      end
+
+      def removeBelow(variable, value) do
+        backend_op(:removeBelow, variable, value)
+      end
+
+      def fix(variable, value) do
+        backend_op(:fix, variable, value)
+      end
+
+      defoverridable domain: 1
+      defoverridable size: 1
+      defoverridable fixed?: 1
+      defoverridable min: 1
+      defoverridable max: 1
+      defoverridable contains?: 2
+      defoverridable remove: 2
+      defoverridable removeAbove: 2
+      defoverridable removeBelow: 2
+      defoverridable fix: 2
+    end
   end
+
+
 
   def topic(variable) do
     [variable.space, variable.id]
@@ -16,5 +93,18 @@ defmodule CPSolver.Variable do
 
   def bind(variable, space) do
     Map.put(variable, :space, space)
+  end
+
+  def set_backend(variable, backend) do
+    Map.put(variable, :backend, backend)
+  end
+
+  def backend_op(op, variable) do
+    apply(variable.backend, op, [variable.space, variable.id])
+  end
+
+  def backend_op(op, variable, value)
+      when op in [:contains?, :remove, :removeAbove, :removeBelow, :fix] do
+    apply(variable.backend, op, [variable.space, variable.id, value])
   end
 end
