cca8239d5351d3a61a3393183b5224c649bd5aea
Boris Okner <boris.okner@gmail.com>
2023-08-29 21:22:13 -0400

    Tests for graph coloring examples

---
+++ test/examples/graph_coloring.exs
@@ -0,0 +1,14 @@
+defmodule CPSolverTest.Examples.GraphColoring do
+  use ExUnit.Case
+
+  test "Triangle" do
+    {:ok, solver} = CPSolver.Examples.GraphColoring.solve("data/graph_coloring/triangle")
+    assert CPSolver.statistics(solver).solution_count == 6
+  end
+
+  test "Square" do
+    {:ok, solver} = CPSolver.Examples.GraphColoring.solve("data/graph_coloring/square")
+    assert CPSolver.statistics(solver).solution_count == 2
+
+  end
+end
