89c74471b29d0457667a7067520defcadd4db125
Boris Okner <boris.okner@gmail.com>
2023-09-06 13:29:55 -0400

    Add P4 example

--- test/examples/graph_coloring_test.exs
+++ test/examples/graph_coloring_test.exs
@@ -2,26 +2,28 @@ defmodule CPSolverTest.Examples.GraphColoring do
   use ExUnit.Case
 
   test "P3" do
-    {:ok, solver} = CPSolver.Examples.GraphColoring.solve("data/graph_coloring/p3")
-    Process.sleep(10)
-    assert CPSolver.statistics(solver).solution_count == 2
+    test_graph("p3", 2)
+  end
+
+  test "P4" do
+    test_graph("p4", 2)
   end
 
   test "Triangle" do
-    {:ok, solver} = CPSolver.Examples.GraphColoring.solve("data/graph_coloring/triangle")
-    Process.sleep(10)
-    assert CPSolver.statistics(solver).solution_count == 6
+    test_graph("triangle", 6)
   end
 
   test "Square" do
-    {:ok, solver} = CPSolver.Examples.GraphColoring.solve("data/graph_coloring/square")
-    Process.sleep(10)
-    assert CPSolver.statistics(solver).solution_count == 2
+    test_graph("square", 2)
   end
 
   test "Triangle and edge" do
-    {:ok, solver} = CPSolver.Examples.GraphColoring.solve("data/graph_coloring/triangle_uni")
-    Process.sleep(100)
-    assert CPSolver.statistics(solver).solution_count == 12
+    test_graph("paw", 12, 100)
+  end
+
+  defp test_graph(graph_name, expected_solutions, timeout \\ 20) do
+    {:ok, solver} = CPSolver.Examples.GraphColoring.solve("data/graph_coloring/#{graph_name}")
+    Process.sleep(timeout)
+    assert CPSolver.statistics(solver).solution_count == expected_solutions
   end
 end
