cd2026a215cf60c9bb823d42a6da3e15f5d167e1
Boris Okner <boris.okner@gmail.com>
2023-09-09 16:00:22 -0400

    More tests for N-queens

--- test/examples/queens_test.exs
+++ test/examples/queens_test.exs
@@ -5,6 +5,18 @@ defmodule CPSolverTest.Examples.Queens do
     test_queens(4, 2)
   end
 
+  test "5 Queens" do
+    test_queens(5, 10, timeout: 2000)
+  end
+
+  test "6 Queens" do
+    test_queens(6, 4, timeout: 2000)
+  end
+
+  test "8 Queens" do
+    test_queens(8, 92, timeout: 2000)
+  end
+
   defp test_queens(n, expected_solutions, opts \\ []) do
     opts = Keyword.merge([timeout: 1000, trials: 1], opts)
 
