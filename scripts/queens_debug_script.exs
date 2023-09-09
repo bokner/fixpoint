c01dc208b8cdeafc3cf01537ebc977a9fc5a23cf
Boris Okner <boris.okner@gmail.com>
2023-09-09 19:06:10 -0400

    Add script for debugging queens

--- scripts/queens_debug_script.exs
+++ scripts/queens_debug_script.exs
@@ -1,22 +1,22 @@
 require Logger
 Logger.configure(level: :error)
 
-instance = "p3"
-expected_sols = 2
+nqueens = 5
+expected_sols = 10
 number_of_runs = 50
 trace_pattern = ["CPSolver.Space"]
 solver_timeout = 1000
 
 
-defmodule DebugGC do
-  def debug(instance, expected_sols, number_of_runs, trace_pattern, solver_timeout \\ 500) do
+defmodule DebugQueens do
+  def debug(nqueens, expected_sols, number_of_runs, trace_pattern, solver_timeout \\ 500) do
 
 
 #trace_pattern = ["CPSolver.Space.create/_", "CPSolver.distribute/_", "CPsolver.Space.shutdown/_"]
 result = Enum.reduce_while(1..number_of_runs, 0, fn i, succ ->
-  #Replbug.start(trace_pattern, time: :timer.seconds(10), msgs: 100_000, max_queue: 10000, silent: true)
+  Replbug.start(trace_pattern, time: :timer.seconds(10), msgs: 100_000, max_queue: 10000, silent: true)
   Process.sleep(100)
-  {:ok, solver} = CPSolver.Examples.GraphColoring.solve("data/graph_coloring/#{instance}")
+  {:ok, solver} = CPSolver.Examples.Queens.solve(nqueens)
   Process.sleep(solver_timeout)
   traces = Replbug.stop()
   Process.sleep(100)
@@ -26,8 +26,5 @@ end)
 end
 end
 
-if result.successes == number_of_runs do
-  Logger.info("All good")
-#else
-#  space_pids = Map.keresult.traces
-end
+
+DebugQueens.solve(nqueens, )
