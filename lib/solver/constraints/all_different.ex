3cf3e70a4e88397d08bdc3deda97df88706d0ffc
Boris Okner <boris.okner@gmail.com>
2023-09-25 17:38:19 -0400

    AllDifferent constraint

---
+++ lib/solver/constraints/all_different.ex
@@ -0,0 +1,14 @@
+defmodule CPSolver.Constraint.AllDifferent do
+  use CPSolver.Constraint
+  alias CPSolver.Propagator.NotEqual
+
+  @impl true
+  def propagators(variables) do
+    for i <- 0..(length(variables) - 2) do
+      for j <- (i + 1)..(length(variables) - 1) do
+        {NotEqual, Enum.at(variables, i), Enum.at(variables, j)}
+      end
+    end
+    |> List.flatten()
+  end
+end
---
+++ test/constraints/all_different_test.exs
@@ -0,0 +1,44 @@
+defmodule CPSolverTest.Constraint.AllDifferent do
+  use ExUnit.Case
+
+  describe "AllDifferent" do
+    alias CPSolver.Propagator.NotEqual, as: PropagatorNotEqual
+    alias CPSolver.Constraint.AllDifferent
+    alias CPSolver.IntVariable
+
+    test "propagators" do
+      domain = 1..3
+      variables = Enum.map(1..3, fn i -> IntVariable.new(domain, name: "x#{i}") end)
+
+      assert variables
+             |> AllDifferent.propagators()
+             |> Enum.map(fn {PropagatorNotEqual, x, y} -> "#{x.name} != #{y.name}" end)
+             |> Enum.sort() ==
+               ["x1 != x2", "x1 != x3", "x2 != x3"]
+    end
+
+    test "produces all possible permutations" do
+      domain = 1..3
+      variables = Enum.map(1..3, fn _ -> IntVariable.new(domain) end)
+
+      model = %{
+        variables: variables,
+        constraints: [{AllDifferent, variables}]
+      }
+
+      {:ok, solver} = CPSolver.solve(model)
+
+      Process.sleep(100)
+      assert CPSolver.statistics(solver).solution_count == 6
+
+      assert CPSolver.solutions(solver) |> Enum.sort() == [
+               [1, 2, 3],
+               [1, 3, 2],
+               [2, 1, 3],
+               [2, 3, 1],
+               [3, 1, 2],
+               [3, 2, 1]
+             ]
+    end
+  end
+end
