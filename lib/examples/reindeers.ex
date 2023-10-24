3dbe618ee3c166669ebb36ff3551c6f9db10a9b5
Boris Okner <boris.okner@gmail.com>
2023-10-23 20:40:04 -0400

    Reindeers

---
+++ lib/examples/reindeers.ex
@@ -0,0 +1,90 @@
+defmodule CPSolver.Examples.Reindeers do
+  @doc """
+  Santa always leaves plans for his elves to determine the order in which the
+  reindeer will pull his sleigh. This year, for the European leg of his
+  journey, his elves are working to the following schedule, which will form a
+  single line of nine reindeer.
+
+  Here are the rules:
+
+    Comet behind Rudolph, Prancer and Cupid
+    Blitzen behind Cupid
+    Blitzen in front of Donder, Vixen and Dancer
+    Cupid in front of Comet, Blitzen and Vixen
+    Donder behind Vixen, Dasher and Prancer
+    Rudolph behind Prancer
+    Rudolph in front of Donder, Dancer and Dasher
+    Vixen in front of Dancer and Comet
+    Dancer behind Donder, Rudolph and Blitzen
+    Prancer in front of Cupid, Donder and Blitzen
+    Dasher behind Prancer
+    Dasher in front of Vixen, Dancer and Blitzen
+    Donder behind Comet and Cupid
+    Cupid in front of Rudolph and Dancer
+    Vixen behind Rudolph, Prancer and Dasher.
+
+  """
+  alias CPSolver.IntVariable, as: Variable
+  alias CPSolver.Constraint.LessOrEqual
+
+  def solve() do
+    reindeers = [
+      Blitzen,
+      Comet,
+      Cupid,
+      Dancer,
+      Dasher,
+      Donder,
+      Prancer,
+      Rudolph,
+      Vixen
+    ]
+
+    domain = 1..length(reindeers)
+
+    vars =
+      [blitzen, comet, cupid, dancer, dasher, donder, prancer, rudolph, vixen] =
+      Enum.map(reindeers, fn r -> Variable.new(domain, name: r) end)
+
+    constraints =
+      behind(comet, [rudolph, prancer, cupid]) ++
+        behind(blitzen, [cupid]) ++
+        in_front_of(blitzen, [donder, vixen, dancer]) ++
+        in_front_of(cupid, [comet, blitzen, vixen]) ++
+        behind(donder, [vixen, dasher, prancer]) ++
+        behind(rudolph, [prancer]) ++
+        in_front_of(rudolph, [donder, dancer, dasher]) ++
+        in_front_of(vixen, [dancer, comet]) ++
+        behind(dancer, [donder, rudolph, blitzen]) ++
+        in_front_of(prancer, [cupid, donder, blitzen]) ++
+        behind(dasher, [prancer]) ++
+        in_front_of(dasher, [vixen, dancer, blitzen]) ++
+        behind(donder, [comet, cupid]) ++
+        in_front_of(cupid, [rudolph, dancer]) ++
+        behind(vixen, [rudolph, prancer, dasher])
+
+    model = %{
+      variables: vars,
+      constraints: constraints
+    }
+
+    {:ok, _solver} =
+      CPSolver.solve(model,
+        solution_handler: fn sol ->
+          sol
+          |> Enum.sort_by(fn {_r, place} -> place end)
+          |> Enum.map(fn {r, _place} -> inspect(r) end)
+          |> Enum.join(" -> ")
+          |> IO.puts()
+        end
+      )
+  end
+
+  defp behind(reindeer, list) do
+    Enum.map(list, fn r -> LessOrEqual.new(reindeer, r, -1) end)
+  end
+
+  defp in_front_of(reindeer, list) do
+    Enum.map(list, fn r -> LessOrEqual.new(r, reindeer, -1) end)
+  end
+end
