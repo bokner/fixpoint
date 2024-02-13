8d44ff07904c7f7dd134ceaeae4da45974631524
Boris Okner <boris.okner@gmail.com>
2024-02-12 22:49:54 -0500

    Minor

--- lib/solver/space/propagation.ex
+++ lib/solver/space/propagation.ex
@@ -53,7 +53,7 @@ defmodule CPSolver.Space.Propagation do
         {p_id, Propagator.filter(p, store: store)}
       end,
       ## TODO: make it an option
-      max_concurrency: 2
+      max_concurrency: 1
     )
     |> Enum.reduce_while({MapSet.new(), graph}, fn {:ok, {p_id, res}}, {scheduled, g} = _acc ->
       case res do
