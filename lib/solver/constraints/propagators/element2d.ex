610e54aae20ba6aa1384e873f5d471c05b78cb50
Boris Okner <boris.okner@gmail.com>
2024-09-04 11:02:02 -0400

    Minor (renaming)

--- lib/solver/constraints/propagators/element2d.ex
+++ lib/solver/constraints/propagators/element2d.ex
@@ -5,48 +5,49 @@ defmodule CPSolver.Propagator.Element2D do
 
   @moduledoc """
   The propagator for Element2D constraint.
+  array2d[row_index][col_index] = value
   """
-  def new(array2d, x, y, z) do
-    new([array2d, x, y, z])
+  def new(array2d, row_index, col_index, value) do
+    new([array2d, row_index, col_index, value])
   end
 
   @impl true
-  def variables([_array2d, x, y, z]) do
+  def variables([_array2d, row_index, col_index, value]) do
     [
-      set_propagate_on(x, :domain_change),
-      set_propagate_on(y, :domain_change),
-      set_propagate_on(z, :domain_change)
+      set_propagate_on(row_index, :domain_change),
+      set_propagate_on(col_index, :domain_change),
+      set_propagate_on(value, :domain_change)
     ]
   end
 
-  defp initial_state([[], _x, _y, _z]) do
+  defp initial_state([[], _row_index, _col_index, _value]) do
     throw(:fail)
   end
 
-  defp initial_state([array2d, x, y, z]) do
+  defp initial_state([array2d, row_index, col_index, value]) do
     num_rows = length(array2d)
     num_cols = length(hd(array2d))
 
-    initial_reduction(array2d, x, y, z, num_rows, num_cols)
-    build_state(array2d, x, y, z, num_rows, num_cols)
+    initial_reduction(array2d, row_index, col_index, value, num_rows, num_cols)
+    build_state(array2d, row_index, col_index, value, num_rows, num_cols)
   end
 
-  def build_state(array2d, x, y, z, num_rows, num_cols) do
+  def build_state(array2d, row_index, col_index, value, num_rows, num_cols) do
     ## Build a state graph.
-    ## Three sets of vertices: ([{:z, value}], [{:x, value}], [{:y, value}])
-    ## with edges from {:z, z_value} to {:x, x_value},
+    ## Three sets of vertices: ([{:value, value}], [{:row_index, value}], [{:col_index, value}])
+    ## with edges from {:value, z_value} to {:row_index, x_value},
     ## where z_value is present in x_value row of array2d.
-    ## Likewise, with edges from {:z, z_value} to {:y, y_value},
+    ## Likewise, with edges from {:value, z_value} to {:col_index, y_value},
     ## where z_value is present in y_value column of array2d.
     for i <- 0..(num_rows - 1), j <- 0..(num_cols - 1), reduce: Graph.new() do
       acc ->
-        if contains?(x, i) && contains?(y, j) do
+        if contains?(row_index, i) && contains?(col_index, j) do
           table_value = Enum.at(array2d, i) |> Enum.at(j)
 
-          if contains?(z, table_value) do
+          if contains?(value, table_value) do
             acc
-            |> Graph.add_edge({:z, table_value}, {:x, i}, label: {:y, j})
-            |> Graph.add_edge({:z, table_value}, {:y, j}, label: {:x, i})
+            |> Graph.add_edge({:value, table_value}, {:row_index, i}, label: {:col_index, j})
+            |> Graph.add_edge({:value, table_value}, {:col_index, j}, label: {:row_index, i})
           else
             acc
           end
@@ -56,37 +57,37 @@ defmodule CPSolver.Propagator.Element2D do
     end
   end
 
-  defp initial_reduction(array2d, x, y, z, num_rows, num_cols) do
+  defp initial_reduction(array2d, row_index, col_index, value, num_rows, num_cols) do
     # x and y are indices in array2d,
     # so we trim D(x) and D(y) accordingly.
-    removeBelow(x, 0)
-    removeAbove(x, num_rows - 1)
-    removeBelow(y, 0)
-    removeAbove(y, num_cols - 1)
-    ## D(z) is bounded by min and max of the array2d
+    removeBelow(row_index, 0)
+    removeAbove(row_index, num_rows - 1)
+    removeBelow(col_index, 0)
+    removeAbove(col_index, num_cols - 1)
+    ## D(value) is bounded by min and max of the array2d
     {arr_min, arr_max} = array2d_min_max(array2d)
-    removeAbove(z, arr_max)
-    removeBelow(z, arr_min)
+    removeAbove(value, arr_max)
+    removeBelow(value, arr_min)
   end
 
-  defp maybe_reduce_domains(x, y, z, %Graph{} = graph) do
-    (maybe_fix(x, y, z, graph) && :passive) ||
+  defp maybe_reduce_domains(row_index, col_index, value, %Graph{} = graph) do
+    (maybe_fix(row_index, col_index, value, graph) && :passive) ||
       (
         {updated_graph, changed?} =
           Enum.reduce(Graph.vertices(graph), {graph, false}, fn
-            {:z, _} = v, acc ->
-              maybe_remove_vertex(v, z, acc)
+            {:value, _} = v, acc ->
+              maybe_remove_vertex(v, value, acc)
 
-            {:x, _} = v, acc ->
-              maybe_remove_vertex(v, x, acc)
+            {:row_index, _} = v, acc ->
+              maybe_remove_vertex(v, row_index, acc)
 
-            {:y, _} = v, acc ->
-              maybe_remove_vertex(v, y, acc)
+            {:col_index, _} = v, acc ->
+              maybe_remove_vertex(v, col_index, acc)
           end)
 
         ## Repeat if any reductions were made
         if changed? do
-          maybe_reduce_domains(x, y, z, updated_graph)
+          maybe_reduce_domains(row_index, col_index, value, updated_graph)
         else
           updated_graph
         end
@@ -112,11 +113,11 @@ defmodule CPSolver.Propagator.Element2D do
     end
   end
 
-  defp remove_vertex(graph, {:z, _value} = vertex) do
+  defp remove_vertex(graph, {:value, _value} = vertex) do
     Graph.delete_vertex(graph, vertex)
   end
 
-  defp remove_vertex(graph, {signature, _value} = vertex) when signature in [:x, :y] do
+  defp remove_vertex(graph, {signature, _value} = vertex) when signature in [:row_index, :col_index] do
     graph
     |> Graph.delete_vertex(vertex)
     ## We delete all edges related to this vertex (that is, labelled {:signature, value})
@@ -158,20 +159,20 @@ defmodule CPSolver.Propagator.Element2D do
     end
   end
 
-  defp filter_impl([_array2d, x, y, z], state) do
-    maybe_reduce_domains(x, y, z, state)
+  defp filter_impl([_array2d, row_index, col_index, value], state) do
+    maybe_reduce_domains(row_index, col_index, value, state)
   end
 
-  defp maybe_fix(x, y, z, graph) do
+  defp maybe_fix(row_index, col_index, value, graph) do
     ## If any 2 are fixed, fix the 3rd
     case Graph.vertices(graph) do
       [_vertex1, _vertex2, _vertex3] = triple ->
         Enum.each(
           triple,
           fn
-            {:x, x_value} -> fix(x, x_value)
-            {:y, y_value} -> fix(y, y_value)
-            {:z, z_value} -> fix(z, z_value)
+            {:row_index, x_value} -> fix(row_index, x_value)
+            {:col_index, y_value} -> fix(col_index, y_value)
+            {:value, z_value} -> fix(value, z_value)
           end
         )
 
