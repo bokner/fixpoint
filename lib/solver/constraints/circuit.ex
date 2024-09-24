bd8eb940486ce0a830eb2299b7cc7e0c09bc4edb
Boris Okner <boris.okner@gmail.com>
2024-09-24 12:47:44 -0400

    Put back all_different propagator for Circuit constraint

--- lib/solver/constraints/circuit.ex
+++ lib/solver/constraints/circuit.ex
@@ -1,9 +1,11 @@
 defmodule CPSolver.Constraint.Circuit do
   use CPSolver.Constraint
   alias CPSolver.Propagator.Circuit, as: CircuitPropagator
+  alias CPSolver.Propagator.AllDifferent.FWC, as: AllDifferentPropagator
+
 
   @impl true
   def propagators(x) do
-    [CircuitPropagator.new(x)]
+    [CircuitPropagator.new(x), AllDifferentPropagator.new(x)]
   end
 end
