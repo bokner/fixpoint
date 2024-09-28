1948e1d161fa04619cec45b2337152b2ea9aecc4
Boris Okner <boris.okner@gmail.com>
2024-09-28 13:35:41 -0400

    Don't use AllDifferent with Circuit; might provide an option later

--- lib/solver/constraints/circuit.ex
+++ lib/solver/constraints/circuit.ex
@@ -1,9 +1,7 @@
 defmodule CPSolver.Constraint.Circuit do
   use CPSolver.Constraint
   alias CPSolver.Propagator.Circuit, as: CircuitPropagator
-  alias CPSolver.Propagator.AllDifferent.FWC, as: AllDifferentPropagator
-
-
+  
   @impl true
   def propagators(x) do
     [CircuitPropagator.new(x)]
