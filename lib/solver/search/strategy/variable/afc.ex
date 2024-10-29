7891ccba3ccda8237a30d033d193e6480a730e50
Boris Okner <boris.okner@gmail.com>
2024-10-29 18:50:01 -0400

    In progress

--- lib/solver/search/strategy/variable/afc.ex
+++ lib/solver/search/strategy/variable/afc.ex
@@ -4,35 +4,78 @@ defmodule CPSolver.Search.VariableSelector.AFC do
   (https://www.gecode.org/doc-latest/MPG.pdf, p.8.5.2)
   """
   alias CPSolver.Space
+  alias CPSolver.Shared
 
   @doc """
   Initialize AFC
   """
-  def initialize_afc(space_data) do
+  def initialize(space_data) do
     propagators = space_data.propagators
     afc_table =  :ets.new(__MODULE__, [:set, :public, read_concurrency: true, write_concurrency: false])
     |> tap(fn afc_table ->
       Enum.each(propagators, fn p ->
-      :ets.insert(afc_table, {p.id, 1}) end)
+      :ets.insert(afc_table, {p.id, afc_record(1, 0)}) end)
     end)
 
-    Space.put_shared(space_data, :afc, afc_table)
+    space_data
+    |> Space.get_shared()
+    |> Shared.put_auxillary(:afc, afc_table)
   end
+
   @doc """
   Compute AFC of variable based on the initial constraint graph
   """
-  def variable_afc(variable, space_data) do
+  def variable_afc(variable, shared) do
 
   end
 
   @doc """
-  Get current AFC of the propagator
+  Recompute AFC based on last AFC value, decay and current global failure count.
+  This is (to be) used:
+  - for computing variable AFC;
+  - for updating AFC of a failed propagator;
+  - for updating AFCs of all propagators in case decay value has been dynamically changed.
   """
-  def propagator_afc(propagator_id, space_data) when is_reference(propagator_id) do
+  def propagator_afc({afc_value, last_update_at} = afc, decay, global_failure_count, failure? \\ false) when decay > 0 and decay < 1 do
+    ## Catch up on decaying (we do not update non-failing propagators on failure event!)
+    ## We also assume that total failure count includes the last failure across the search nodes.
+    decay_steps = max(0, if failure? do
+      global_failure_count - last_update_at - 1
+    else
+      global_failure_count - last_update_at
+    end)
+
+    ## It's impractical to consider more than 100 (probably less) decaying steps - the values wwill be very
+    ## close to 0 even for big global failure counts.
+    #
+    ## Add 1 to decayed AFC of failing propagator
+    new_value = afc_value * :math.pow(decay, min(100, decay_steps)) + (failure? && 1 || 0)
+
+    {new_value, global_failure_count}
+
 
   end
 
-  def update_afc(failed_propagator_id, space_data) do
+  defp afc_record(afc_value, last_failure_at) do
+    {afc_value, last_failure_at}
+  end
 
+  ## Get AFC propagator record
+  def get_afc_record(table, propagator_id) when is_reference(table) do
+    table
+    |> :ets.lookup(propagator_id)
+    |> then(fn rec -> !Enum.empty?(rec) && elem(hd(rec), 1) || nil end)
+  end
+
+  def get_afc_record(space_data, propagator_id) do
+    space_data
+    |> get_afc_table()
+    |> get_afc_record(propagator_id)
+  end
+
+  def get_afc_table(space_data) do
+    space_data
+    |> Space.get_shared()
+    |> Shared.get_auxillary(:afc)
   end
 end
---
+++ test/search/afc_test.exs
@@ -0,0 +1,37 @@
+defmodule CPSolverTest.Search.AFC do
+  use ExUnit.Case
+
+  alias CPSolver.Search.VariableSelector.AFC, as: AFC
+  import CPSolver.Test.Helpers
+
+  describe "AFC search strategy" do
+    alias CPSolver.Shared
+
+    test "initialization from top space" do
+      space = build_space()
+      AFC.initialize(space)
+      afc_table_ref = AFC.get_afc_table(space)
+      propagator_refs = Enum.map(space.propagators, fn p -> p.id end)
+
+      assert Enum.all?(
+               :ets.tab2list(afc_table_ref),
+               fn {p_ref, {1, 0}} -> p_ref in propagator_refs end
+             )
+    end
+
+    test "get AFC record" do
+      space = build_space()
+      AFC.initialize(space)
+      propagator_refs = Enum.map(space.propagators, fn p -> p.id end)
+
+      assert Enum.all?(propagator_refs, fn p_id ->
+               {1, 0} = AFC.get_afc_record(space, p_id)
+             end)
+    end
+
+    defp build_space() do
+      shared = Shared.init_shared_data(space_threads: 4)
+      space_setup(0..5, 0..5, 0..5) |> Map.put(:opts, solver_data: shared)
+    end
+  end
+end
