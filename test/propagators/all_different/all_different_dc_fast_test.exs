31b17e8c865f39eb65d2465000f3d85b1a7232bb
Boris Okner <boris.okner@gmail.com>
2025-01-15 23:40:46 -0500

    More unit tests for fast reduction algo

--- test/propagators/all_different/all_different_dc_fast_test.exs
+++ test/propagators/all_different/all_different_dc_fast_test.exs
@@ -20,35 +20,28 @@ defmodule CPSolverTest.Propagator.AllDifferent.DC.Fast do
         assert Interface.min(x3) == 4 && Interface.max(x3) == 5
     end
 
-    # test "cascading filtering" do
-    #   ## all variables become fixed, and this will take a single filtering call.
-    #   ##
-    #   x =
-    #     Enum.map([{"x2", 1..2}, {"x1", 1}, {"x3", 1..3}, {"x4", 1..4}, {"x5", 1..5}], fn {name, d} ->
-    #       Variable.new(d, name: name)
-    #     end)
+    test "cascading filtering" do
+      ## all variables become fixed, and this will take a single filtering call.
+      ##
+      [x2, _x1, x3, x4, x5] = vars =
+        Enum.map([{"x2", 1..2}, {"x1", 1}, {"x3", 1..3}, {"x4", 1..4}, {"x5", 1..5}], fn {name, d} ->
+          Variable.new(d, name: name)
+        end)
 
-    #   {:ok, x_vars, _store} = ConstraintStore.create_store(x)
+      Fast.reduce(vars)
+      assert Interface.fixed?(x2) && Interface.min(x2) == 2
+      assert Interface.fixed?(x3) && Interface.min(x3) == 3
+      assert Interface.fixed?(x4) && Interface.min(x4) == 4
+      assert Interface.fixed?(x5) && Interface.min(x5) == 5
 
-    #   dc_propagator = DC.new(x_vars)
-    #   %{changes: changes, active?: active?} = Propagator.filter(dc_propagator)
-    #   ## The propagators is passive
-    #   refute active?
-    #   assert map_size(changes) == Arrays.size(x_vars) - 1
-    #   assert Enum.all?(Map.values(changes), fn change -> change == :fixed end)
-    #   ## All variables are now fixed
-    #   assert Enum.all?(x_vars, &Interface.fixed?/1)
-    # end
+    end
 
-    # test "inconsistency (pigeonhole)" do
-    #   domains = List.duplicate(1..3, 4)
+    test "inconsistency (pigeonhole)" do
+      domains = List.duplicate(1..3, 4)
 
-    #   vars =
-    #     Enum.map(domains, fn d -> Variable.new(d) end)
-
-    #   {:ok, bound_vars, _store} = CPSolver.ConstraintStore.create_store(vars)
-    #   dc_propagator = DC.new(bound_vars)
-    #   assert Propagator.filter(dc_propagator) == :fail
-    # end
+      vars =
+        Enum.map(domains, fn d -> Variable.new(d) end)
+      assert catch_throw(Fast.reduce(vars)) == :fail
+    end
   end
 end
