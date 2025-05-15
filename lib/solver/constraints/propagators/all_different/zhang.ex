baf45c08f7bd118f8555657e7a94ff3538660e05
Boris Okner <boris.okner@gmail.com>
2025-05-15 16:01:01 -0400

    DCFast: pass tests and Sudoku; not clear on Queens

--- lib/solver/constraints/propagators/all_different/zhang.ex
+++ lib/solver/constraints/propagators/all_different/zhang.ex
@@ -65,16 +65,18 @@ defmodule CPSolver.Propagator.AllDifferent.Zhang do
   end
 
   def remove_type2_edges(%{GA_complement_matching: matching} = state, remove_edge_fun) do
-    state
-    |> flip_matching_edges()
-    |> process_sccs(matching, remove_edge_fun)
-    |> then(fn {scc_components, reduced_graph} ->
+    (Enum.empty?(matching) && state) ||
       state
-      |> Map.put(:value_graph, reduced_graph)
-      |> Map.update!(:components, fn type1_component ->
-        MapSet.size(type1_component) > 0 && [type1_component | scc_components] || scc_components end)
-    end)
-
+      |> flip_matching_edges()
+      |> process_sccs(matching, remove_edge_fun)
+      |> then(fn {scc_components, reduced_graph} ->
+        state
+        |> Map.put(:value_graph, reduced_graph)
+        |> Map.update!(:components, fn type1_component ->
+          (MapSet.size(type1_component) > 0 && [type1_component | scc_components]) ||
+            scc_components
+        end)
+      end)
   end
 
   defp flip_matching_edges(%{value_graph: graph, GA_complement_matching: matching} = _state) do
@@ -93,20 +95,21 @@ defmodule CPSolver.Propagator.AllDifferent.Zhang do
         end),
       component_handler:
         {fn component, {component_acc, graph_acc} = _handler_acc ->
-           Enum.reduce(component, {component_acc, graph_acc}, fn vertex_index, {c_acc, g_acc} = acc ->
+           Enum.reduce(component, {component_acc, graph_acc}, fn vertex_index,
+                                                                 {c_acc, g_acc} = acc ->
              case BitGraph.V.get_vertex(graph_acc, vertex_index) do
                ## We only need to remove out-edges from 'variable' vertices
                ## that cross to other SCCS
                {:variable, _variable_id} = v ->
                  foreign_neighbors = BitGraph.E.out_neighbors(g_acc, vertex_index)
 
-                 {MapSet.size(component) > 1 && [component | c_acc] || c_acc, ## drop 1-vertex sccs
-                 Enum.reduce(foreign_neighbors, g_acc, fn neighbor, g_acc2
-                                                              when is_integer(neighbor) ->
-                   (neighbor in component && g_acc2) ||
-                     remove_edge_fun.(g_acc2, v, BitGraph.V.get_vertex(g_acc2, neighbor))
-                 end)
-                }
+                 ## drop 1-vertex sccs
+                 {(MapSet.size(component) > 1 && [component | c_acc]) || c_acc,
+                  Enum.reduce(foreign_neighbors, g_acc, fn neighbor, g_acc2
+                                                           when is_integer(neighbor) ->
+                    (neighbor in component && g_acc2) ||
+                      remove_edge_fun.(g_acc2, v, BitGraph.V.get_vertex(g_acc2, neighbor))
+                  end)}
 
                {:value, _} ->
                  acc
