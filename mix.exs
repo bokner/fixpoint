b0b3f21fcc5f491549c483d56f465ed7e6043c31
Boris Okner <boris.okner@gmail.com>
2025-07-14 10:04:08 -0400

    Update bitgraph; Queens to use DC.Fast

--- mix.exs
+++ mix.exs
@@ -28,6 +28,7 @@ defmodule CPSolver.MixProject do
     [
       {:libgraph, "~> 0.16.0"},
       {:bitgraph, "~> 0.1"},
+      #{:bitgraph, path: Path.join([System.get_env("HOME"), "projects", "bitgraph"])},
       {:arrays, "~> 2.1"},
       {:arrays_aja, "~> 0.2.0"},
       {:math, "~> 0.7.0", only: :test},
