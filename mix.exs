6736a22fa0e4719e1b37373fff9838ba97902fda
Boris Okner <boris.okner@gmail.com>
2025-08-04 22:51:38 -0400

    v0.12.8

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CPSolver.MixProject do
   def project do
     [
       app: :fixpoint,
-      version: "0.12.7",
+      version: "0.12.8",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -28,7 +28,6 @@ defmodule CPSolver.MixProject do
     [
       {:libgraph, "~> 0.16.0"},
       {:bitgraph, "~> 0.1"},
-      #{:bitgraph, path: Path.join([System.get_env("HOME"), "projects", "bitgraph"])},
       {:arrays, "~> 2.1"},
       {:arrays_aja, "~> 0.2.0"},
       {:math, "~> 0.7.0", only: :test},
