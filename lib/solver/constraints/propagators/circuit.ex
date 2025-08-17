57396a612b16acf7b6a9308eaa249928d833287e
Boris Okner <boris.okner@gmail.com>
2025-08-16 21:24:20 -0400

    Fix (missing alias)

--- lib/solver/constraints/propagators/circuit.ex
+++ lib/solver/constraints/propagators/circuit.ex
@@ -1,7 +1,7 @@
 defmodule CPSolver.Propagator.Circuit do
   use CPSolver.Propagator
 
-  alias Iter.{Iterable, Iterable.FlatMapper}
+  alias Iter.{Iterable, Iterable.FlatMapper, Iterable.Mapper}
 
   @moduledoc """
   The propagator for 'circuit' constraint.
@@ -152,8 +152,8 @@ defmodule CPSolver.Propagator.Circuit do
 
   defp neighbor_finder(vars) do
     fn _graph, vertex_index, :out ->
-        vars 
-        |> get_variable(vertex_index - 1) 
+        vars
+        |> get_variable(vertex_index - 1)
         |> domain_iterator()
       _graph, vertex_index, :in ->
         FlatMapper.new(1..Arrays.size(vars),
