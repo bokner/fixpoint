f1a8a5520e74a05d3eeee763b32995c7f06bb20d
Boris Okner <boris.okner@gmail.com>
2025-08-20 19:31:42 -0400

    v0.13.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CPSolver.MixProject do
   def project do
     [
       app: :fixpoint,
-      version: "0.13.4",
+      version: "0.13.5",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
