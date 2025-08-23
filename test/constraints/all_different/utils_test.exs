8c700c55e4a07b2a67d05d8a166278c497d45ec0
Boris Okner <boris.okner@gmail.com>
2025-08-22 20:57:12 -0400

    More unit tests for FWC

--- test/constraints/all_different/utils_test.exs
+++ test/constraints/all_different/utils_test.exs
@@ -28,5 +28,26 @@ defmodule CPSolverTest.Constraint.AllDifferent.Utils do
       Variable.fix(Enum.random(vars), Enum.random(1..2))
       assert catch_throw(:fail = AllDiffUtils.forward_checking(vars))
     end
+
+    test "reuse" do
+      # An example from Zhang
+      domains = [1, 1..2, 1..4, [1, 2, 4, 5]]
+      vars = Enum.map(domains, fn d -> Variable.new(d) end)
+      {unfixed_indices, fixed_values} = AllDiffUtils.forward_checking(vars)
+      ## First 2 variables fixed with values 1 and 2
+      assert unfixed_indices == MapSet.new([2, 3])
+      assert fixed_values == MapSet.new([1, 2])
+      ## Run FWC again with the data from previous run - no effect
+      assert {unfixed_indices, fixed_values} == AllDiffUtils.forward_checking(vars, unfixed_indices, fixed_values)
+      ## Fix and run FWC again with previous results
+      ##
+      ## domain(var3) = [3,4]; domain(var4) = [4,5]
+      ## Fix shared value (4) for any of the unfixed variables
+      Variable.fix(Enum.at(vars, Enum.random([2,3])), 4)
+      {unfixed_indices2, _fixed_values2} = AllDiffUtils.forward_checking(vars, unfixed_indices, fixed_values)
+      ## Everything is fixed
+      assert Enum.empty?(unfixed_indices2)
+      assert Enum.all?(vars, fn var -> Variable.fixed?(var) end)
+    end
   end
 end
