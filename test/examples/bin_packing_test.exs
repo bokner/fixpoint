bf56397377003993bb9b2ce5b6936ee9061c355f
Boris Okner <boris.okner@gmail.com>
2026-01-10 11:44:17 -0500

    Bin packing (changes to PR from Lorenzo)

---
+++ test/examples/bin_packing_test.exs
@@ -0,0 +1,56 @@
+defmodule CPSolverTest.Examples.BinPacking do
+  use ExUnit.Case
+
+  alias CPSolverTest.Examples.BinPacking
+  alias CPSolver.Examples.BinPacking
+
+  test "small binpacking" do
+    test_bin_packing("p01")
+  end
+
+  test "med binpacking" do
+    test_bin_packing("p02")
+  end
+
+  test "big binpacking" do
+    test_bin_packing("p03")
+  end
+
+  test "big binpacking 2" do
+    test_bin_packing("p04")
+  end
+
+  defp test_bin_packing(dataset) do
+    weights =
+      File.read!("data/bin_packing/#{dataset}/#{dataset}_w.txt")
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn line ->
+        line
+        |> String.trim()
+        |> String.to_integer()
+      end)
+
+    max_capacity =
+      File.read!("data/bin_packing/#{dataset}/#{dataset}_c.txt")
+      |> String.trim()
+      |> String.to_integer()
+
+    expected_solution =
+      File.read!("data/bin_packing/#{dataset}/#{dataset}_s.txt")
+      |> String.split("\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.with_index()
+      |> Enum.reduce(%{}, fn {bin_1_based, item_0_based}, acc ->
+        bin_0_based = bin_1_based - 1
+        Map.update(acc, bin_0_based, [item_0_based], fn items -> [item_0_based | items] end)
+      end)
+
+    model = BinPacking.model(weights, max_capacity, :minimize)
+    {:ok, result} = CPSolver.solve(model, search: {:first_fail, :indomain_max})
+    assert_solutions(result, weights, max_capacity)
+  end
+
+  defp assert_solutions(result, weights, max_capacity) do
+    assert BinPacking.check_solution(result, weights, max_capacity)
+  end
+end
