55d6fda52376dfd204949c600e442e18e0f2955b
Boris Okner <boris.okner@gmail.com>
2026-05-17 10:37:47 -0400

    Minor (on the way to refactor more search)

--- lib/solver/search/utils.ex
+++ lib/solver/search/utils.ex
@@ -4,6 +4,7 @@ defmodule CPSolver.Search.Utils do
   ## Pick all minimal elements according to given minimization function
   def minimals(data, min_by_fun) do
     %{initial: initial, reducer_fun: reducer_fun} = minimals_reducer(min_by_fun)
+
     Tracker.iterate(data, initial, reducer_fun, false)
     |> elem(0)
   end
@@ -11,6 +12,7 @@ defmodule CPSolver.Search.Utils do
   ## Pick all maximal elements according to given maximization function
   def maximals(data, max_by_fun) do
     %{initial: initial, reducer_fun: reducer_fun} = maximals_reducer(max_by_fun)
+
     Tracker.iterate(data, initial, reducer_fun, false)
     |> elem(0)
   end
@@ -19,33 +21,36 @@ defmodule CPSolver.Search.Utils do
     make_reducer(
       {[], nil},
       fn var, {minimals_acc, current_min} = acc ->
-      val = min_by_fun.(var)
+        val = min_by_fun.(var)
 
-      cond do
-        is_nil(current_min) || val < current_min -> {[var], val}
-        is_nil(val) || val > current_min -> acc
-        val == current_min -> {[var | minimals_acc], current_min}
+        cond do
+          is_nil(current_min) || val < current_min -> {[var], val}
+          is_nil(val) || val > current_min -> acc
+          val == current_min -> {[var | minimals_acc], current_min}
+        end
       end
-    end)
-  end
-
-  def maximals_reducer(max_by_fun) do
-    make_reducer({
-      [], -1},
-      fn var, {maximals_acc, current_max} = acc ->
-      val = max_by_fun.(var)
-
-      cond do
-        is_nil(val) || val < current_max -> acc
-        val > current_max -> {[var], val}
-        val == current_max -> {[var | maximals_acc], val}
-      end
-    end
     )
   end
 
-  def make_reducer(initial, reducer_fun) do
-    %{initial: initial, reducer_fun: reducer_fun}
+  def maximals_reducer(max_by_fun) do
+    make_reducer(
+      {
+        [],
+        -1
+      },
+      fn var, {maximals_acc, current_max} = acc ->
+        val = max_by_fun.(var)
+
+        cond do
+          is_nil(val) || val < current_max -> acc
+          val > current_max -> {[var], val}
+          val == current_max -> {[var | maximals_acc], val}
+        end
+      end
+    )
   end
 
+  def make_reducer(initial, reducer_fun, postprocess_fun \\ fn x -> elem(x, 0) end) do
+    %{initial: initial, reducer_fun: reducer_fun, postprocess_fun: postprocess_fun}
+  end
 end
