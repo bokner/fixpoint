0253b443b1fc6e260fcb8ffd0b317df489619935
Boris Okner <boris.okner@gmail.com>
2026-08-17 17:50:22 -0400

    wip

--- lib/solver/search/lns/lns.ex
+++ lib/solver/search/lns/lns.ex
@@ -1,2 +1,12 @@
 defmodule CPSolver.Search.LNS do
+  @callback init(model :: map()) :: :todo
+  @callback destroy(model :: map(), solution :: map()) :: :todo
+
+  def run(opts) do
+    initial_solution = Keyword.get(opts, :initial_solution)
+    implementation = Keyword.get(opts, :impl)
+    stop_condition = Keyword.get(opts, :stop_condition)
+  end
+
+
 end
