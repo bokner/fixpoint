3e534164277fa815d9895e920d70188aaeb2caf1
Boris Okner <boris.okner@gmail.com>
2026-09-09 20:30:52 -0400

    Wip

--- lib/solver/search/lns/lns.ex
+++ lib/solver/search/lns/lns.ex
@@ -1,7 +1,23 @@
 defmodule CPSolver.Search.LNS do
   @callback init(model :: map()) :: :todo
-  @callback destroy(model :: map(), solution :: map()) :: :todo
+  @callback destroy(solution :: map()) :: :todo
+  @callback repair(solution :: map()) :: :todo
+  @callback accept(current_solution :: map(), new_solution :: map()) :: :todo
 
+  @doc """
+    input: a feasible solution x
+    2: xb = x;
+    3: repeat
+    4: xt = r(d(x));
+    5: if accept(xt , x) then
+    6: x = xt ;
+    7: end if
+    8: if c(xt ) < c(xb) then
+    9: xb = xt ;
+    10: end if
+    11: until stop criterion is met
+    12: return xb
+  """
   def run(opts) do
     initial_solution = Keyword.get(opts, :initial_solution)
     implementation = Keyword.get(opts, :impl)
